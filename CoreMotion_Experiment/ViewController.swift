e1790a86b42f85390be4a4d4d23bdf9d32192561
Gregory Keeley <gregoryKeeley@pursuit.org>
2020-12-04 14:48:03 -0500

    - Added "Privacy - Motion" to the plist - Setup basic UI to display motion readings - Setup basic framework to start reading motion (from headphones)

--- CoreMotion_Experiment/ViewController.swift
+++ CoreMotion_Experiment/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var attitudeValueLabel: UILabel!
+    @IBOutlet weak var gravityValueLabel: UILabel!
+    @IBOutlet weak var headingValueLabel: UILabel!
+    @IBOutlet weak var magneticFieldValueLabel: UILabel!
+    @IBOutlet weak var rotationRateValueLabel: UILabel!
+    @IBOutlet weak var sensorLocationValueLabel: UILabel!
+    @IBOutlet weak var userAccelerationValueLabel: UILabel!
+    
+    var motionManager = CMHeadphoneMotionManager()
+    
+    var motionData: CMDeviceMotion?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        startMotionServices()
     }
-
-
+    
+    func startMotionServices() {
+        motionManager.delegate = self
+        motionManager.startDeviceMotionUpdates()
+    }
+    func motionValueChanged() {
+        if let accelerometerData = motionManager.deviceMotion {
+            attitudeValueLabel.text = accelerometerData.attitude.description
+            gravityValueLabel.text = "X: \(accelerometerData.gravity.x.description)\n Y: \(accelerometerData.gravity.y.description)\n Z: \(accelerometerData.gravity.z.description)"
+            headingValueLabel.text = accelerometerData.heading.description
+            magneticFieldValueLabel.text = "X: \(accelerometerData.magneticField.field.x)\n Y: \(accelerometerData.gravity.y.description)\n Z: \(accelerometerData.gravity.z.description)"
+            sensorLocationValueLabel.text = accelerometerData.sensorLocation.rawValue.description
+            userAccelerationValueLabel.text = "X: \(accelerometerData.userAcceleration.x.description)\n Y: \(accelerometerData.userAcceleration.x.description)\n Z: \(accelerometerData.userAcceleration.x.description)"
+        } else {
+            print("No accelerometerData")
+        }
+    }
 }
 
+extension ViewController: CMHeadphoneMotionManagerDelegate {
+    func headphoneMotionManagerDidConnect(_ manager: CMHeadphoneMotionManager) {
+        if motionManager.isDeviceMotionAvailable {
+
+        } else {
+            
+        }
+    }
+    func headphoneMotionManagerDidDisconnect(_ manager: CMHeadphoneMotionManager) {
+        print("Headphones disconnected")
+    }
+    
+}
